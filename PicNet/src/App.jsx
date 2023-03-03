01462654959ff08d849b683ed06d4d7f79b206b5
Andres Hidalgo <andres_fel.hidalgo@uao.edu.co>
2023-03-03 00:11:04 -0500

    PostTest

--- PicNet/src/App.jsx
+++ PicNet/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Post } from "./Components/Post";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
     <>
+      <Post />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
