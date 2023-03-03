53f703a59eb3e96faebe942195c58fbef3c35464
Andres Hidalgo <andres_fel.hidalgo@uao.edu.co>
2023-03-02 23:56:34 -0500

    Post component

---
+++ PicNet/src/Components/Post.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "../App.css";
+export const Post = () => {
+  return (
+    <>
+      <div className="namePicUser">
+        <img
+          className="user"
+          src="https://robohash.org/user1"
+          alt="profile-photo"
+        />
+        <p>nombreusuario</p>
+      </div>
+
+      <img src="http://ximg.es/400x400/000/fff" alt="" />
+    </>
+  );
+};
