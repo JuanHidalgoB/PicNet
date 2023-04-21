9a1475e7c7c639a158614f318972c9afd47068b5
Juan Hidalgo <juan.hidalgo@uao.edu.co>
2023-04-20 21:16:37 -0500

    Componente NavBar

---
+++ PicNet/src/Components/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../Styles/NavBar.css';
+
+function NavBar() {
+    return (
+      <div className="nav">
+        <img src={logo} alt="Logo" />
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+              <a href="/search">Search</a>
+          </li>
+          <li>
+            <a href="/chats">Chats</a>
+          </li>
+          <li>
+            <a href="/create">Create</a>
+          </li>
+          <li>
+            <a href="/profile">Profile</a>
+          </li>
+          <li>
+            <a href="/logout">Log out</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default NavBar
